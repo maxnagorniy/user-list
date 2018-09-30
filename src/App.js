ede883e5a93f23819f9c0a7198d65504338ab7e3
Maxim <max1m_ka@ukr.net>
2018-10-01 01:27:20 +0300

    search in all respects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Item } from 'semantic-ui-react';
+import { Item, Form, Icon } from 'semantic-ui-react';
 
 
 
@@ -10,48 +10,74 @@ import { Item } from 'semantic-ui-react';
 //     }
 // }
 
+const isMatch = (user, filter) =>
+    new RegExp(filter, "i").test(JSON.stringify(user));
+
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.searchHandler = this.searchHandler.bind(this);
-    }
+  constructor(props){
+    super(props);
+    this.state = {
+      show: true
+    };
+    this.searchHandler = this.searchHandler.bind(this);
+    // this.toggleAside = this.toggleAside.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+  }
 
   searchHandler() {
-      console.log('findUser', this.searchInput.value);
-      this.props.onFindPeople(this.searchInput.value)
+    console.log('findUser', this.searchInput.value);
+    this.props.onFindPeople(this.searchInput.value)
+  }
+
+  handleClick() {
+      console.log('OK');
+  }
+
+  toggleAside() {
+      this.setState({
+          show: !this.state.show
+      })
   }
 
   render() {
       console.log(this.props.testStore);
     return (
       <div className="App">
-          <aside>
-              <form action="#">
-                  <input type="text" ref={(input) => {this.searchInput = input}}
-                        onChange={this.searchHandler}
-                  />
-              </form>
+          <aside className={this.state.show ? 'show' : 'hidden'}>
+              <Form>
+                  <Form.Field>
+                      <input type="text" ref={(input) => {
+                          this.searchInput = input
+                      }}
+                             onChange={this.searchHandler}
+                             placeholder="Start typing..."
+                      />
+                  </Form.Field>
+              </Form>
               <Item.Group link>
                   {this.props.testStore.map((arrayItem, index) =>
 
-                      <Item key={index} id={index} onClick={this.handleClick}>
-                          <Item.Image size='tiny' src={arrayItem.general.avatar} />
+                      <Item key={index} id={index} onClick={()=>{this.handleClick(); this.toggleAside()}}>
+                          <Item.Image size='tiny' src={arrayItem.general.avatar}/>
 
                           <Item.Content>
                               <Item.Header>{`${arrayItem.general.firstName} ${arrayItem.general.lastName}`}</Item.Header>
+                              <Item.Description>{arrayItem.job.company}</Item.Description>
                           </Item.Content>
                       </Item>
                   )}
               </Item.Group>
           </aside>
-          <main>
-              {this.props.testStore.map((arrayItem, index) =>
 
-                  <div key={index}>
-                      <img src={arrayItem.general.avatar} alt=""/>
-                      <p>{arrayItem.address.street}</p>
-                  </div>
-              )}
+          <main>
+              <button type="button" onClick={() => this.toggleAside()}><Icon name='bars' size='large' /></button>
+              {/*{this.props.testStore.map((arrayItem, index) =>*/}
+
+                  {/*<div key={index}>*/}
+                      {/*<img src={arrayItem.general.avatar} alt=""/>*/}
+                      {/*<p>{arrayItem.address.street}</p>*/}
+                  {/*</div>*/}
+              {/*)}*/}
           </main>
       </div>
     );
@@ -60,7 +86,7 @@ class App extends Component {
 
 export default connect(
     state => ({
-        testStore: state.allPeople.filter(arrayItem => arrayItem.general.firstName.includes(state.filterPeople))
+        testStore: state.allPeople.filter(user => isMatch(user, state.filterPeople))
     }),
     dispatch => ({
         onFindPeople: (firstName) => {
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './App';
-import './index.css';
 import reduser from './redusers';
+import './index.css';
 
 
 import registerServiceWorker from './registerServiceWorker';
